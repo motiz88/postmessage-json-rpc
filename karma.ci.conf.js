7c59c16cf38334fa029c3b168261c5580ab1a4a6
motiz88 <motiz88@gmail.com>
2015-07-09 23:46:16 +0300

    Set up coveralls.io in CI

--- karma.ci.conf.js
+++ karma.ci.conf.js
@@ -4,6 +4,11 @@ var path = require('path');
 module.exports = function(config) {
     baseconf(config);
     config.set({
+        reporters: ['nested', 'coverage', 'coveralls'],
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/'
+        }
         captureTimeout: 300000,
         browsers: ['PhantomJS', 'Firefox'],
         singleRun: true
